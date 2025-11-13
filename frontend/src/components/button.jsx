8aae0c54c3feee732f87eaaf6efb1ddc7c0e1eb8
carbidx2025 <carbidx2025@gmail.com>
2025-11-13 01:59:50 -0800

    Create button.jsx

---
+++ frontend/src/components/button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Button.css";
+
+export default function Button({ children, onClick, type = "button" }) {
+  return (
+    <button className="cbx-btn" type={type} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
