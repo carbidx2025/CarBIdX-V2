01fec01517ad82c3724fd0a1da4c404522909c61
carbidx2025 <carbidx2025@gmail.com>
2025-11-14 15:41:54 -0800

    Update router.jsx

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 /* Pages */
-import Home from "./pages/home/home";
+import Home from "./pages/home";
 import BuyerLogin from "./pages/buyer/buyerlogin";
 import BuyerRegister from "./pages/buyer/buyerregister";
 import BuyerVerify from "./pages/buyer/buyerverify";
