acf08fe0035517ff15b4924a49de737f13ad8b7b
carbidx2025 <carbidx2025@gmail.com>
2025-11-14 00:35:59 -0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./router";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-[#0a0f24] text-white">
+      <div className="min-h-screen bg-[#0a0f24] text-white flex flex-col">
         <Navbar />
-        <AppRouter />
+
+        <div className="flex-grow">
+          <AppRouter />
+        </div>
+
+        <Footer />
       </div>
     </BrowserRouter>
   );
