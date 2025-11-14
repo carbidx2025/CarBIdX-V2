46ac1139dc2f98550046274847a94a1d7f7987cc
carbidx2025 <carbidx2025@gmail.com>
2025-11-14 00:22:14 -0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./router";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="min-h-screen bg-[#0a0f24] text-white">
+        <Navbar />
         <AppRouter />
       </div>
     </BrowserRouter>
