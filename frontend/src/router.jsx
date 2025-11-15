f9a364295ca12857c7dd1a8e75ab087ff267fd98
carbidx2025 <carbidx2025@gmail.com>
2025-11-14 16:18:54 -0800

    Update router.jsx

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,27 +1,35 @@
 import { Routes, Route } from "react-router-dom";
 
-/* Home (located in /pages/home.jsx) */
-import Home from "./pages/home.jsx";
+/* Root pages */
+import Home from "./pages/home";
+import HowItWorks from "./pages/HowItWorks";
+import Contact from "./pages/Contact";
+import Buyers from "./pages/Buyers";
+import Dealers from "./pages/Dealers";
+import Request from "./pages/Request";
+import Verify from "./pages/Verify";
+import Confirm from "./pages/Confirm";
+import AuctionLive from "./pages/AuctionLive";
 
-/* Buyer pages */
-import BuyerLogin from "./pages/Buyer/BuyerLogin.jsx";
-import BuyerRegister from "./pages/Buyer/BuyerRegister.jsx";
-import BuyerVerify from "./pages/Buyer/BuyerVerify.jsx";
-import BuyerRequest from "./pages/Buyer/BuyerRequest.jsx";
-import BuyerDashboard from "./pages/Buyer/Dashboard.jsx";
-import BuyerAuctions from "./pages/Buyer/Auctions.jsx";
-import BuyerAuctionDetail from "./pages/Buyer/AuctionDetail.jsx";
-import BuyerConfirm from "./pages/Buyer/BuyerConfirm.jsx";
+/* Buyer */
+import BuyerLogin from "./pages/Buyer/BuyerLogin";
+import BuyerRegister from "./pages/Buyer/BuyerRegister";
+import BuyerVerify from "./pages/Buyer/BuyerVerify";
+import BuyerRequest from "./pages/Buyer/BuyerRequest";
+import BuyerDashboard from "./pages/Buyer/BuyerDashboard";
+import BuyerAuctions from "./pages/Buyer/BuyerAuctions";
+import BuyerAuctionDetail from "./pages/Buyer/BuyerAuctionDetail";
+import BuyerConfirm from "./pages/Buyer/BuyerConfirm";
 
-/* Dealer pages */
-import DealerLogin from "./pages/Dealer/DealerLogin.jsx";
-import DealerDashboard from "./pages/Dealer/DealerDashboard.jsx";
-import DealerCreateListing from "./pages/Dealer/CreateListing.jsx";
-import DealerMyListings from "./pages/Dealer/MyListings.jsx";
+/* Dealer */
+import DealerLogin from "./pages/Dealer/DealerLogin";
+import DealerDashboard from "./pages/Dealer/DealerDashboard";
+import DealerCreateListing from "./pages/Dealer/DealerCreateListing";
+import DealerMyListings from "./pages/Dealer/DealerMyListings";
 
-/* Admin pages */
-import AdminLogin from "./pages/Admin/Login.jsx";
-import AdminDashboard from "./pages/Admin/Dashboard.jsx";
+/* Admin */
+import AdminLogin from "./pages/Admin/Login";
+import AdminDashboard from "./pages/Admin/Dashboard";
 
 export default function AppRouter() {
   return (
@@ -29,6 +37,14 @@ export default function AppRouter() {
 
       {/* Home */}
       <Route path="/" element={<Home />} />
+      <Route path="/how-it-works" element={<HowItWorks />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/buyers" element={<Buyers />} />
+      <Route path="/dealers" element={<Dealers />} />
+      <Route path="/request" element={<Request />} />
+      <Route path="/verify" element={<Verify />} />
+      <Route path="/confirm" element={<Confirm />} />
+      <Route path="/auction-live" element={<AuctionLive />} />
 
       {/* Buyer */}
       <Route path="/buyer/login" element={<BuyerLogin />} />
