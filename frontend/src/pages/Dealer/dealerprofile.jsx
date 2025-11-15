d3e18e3f05c7700ea932941072b0c8493e18cc90
carbidx2025 <carbidx2025@gmail.com>
2025-11-14 22:12:49 -0800

    Create dealerprofile.jsx

---
+++ frontend/src/pages/Dealer/dealerprofile.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function DealerProfile() {
+  return (
+    <div style={{ padding: "30px", color: "white" }}>
+      <h1>Profile & Settings</h1>
+      <p>Manage dealer account details here.</p>
+    </div>
+  );
+}
