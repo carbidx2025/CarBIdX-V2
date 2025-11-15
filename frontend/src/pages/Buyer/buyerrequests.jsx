ad055e252c84b61cc8307b66bfa6f3bfde38441c
carbidx2025 <carbidx2025@gmail.com>
2025-11-14 22:03:05 -0800

    Create buyerrequests.jsx

---
+++ frontend/src/pages/Buyer/buyerrequests.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function BuyerRequests() {
+  return (
+    <div style={{ padding: "30px", color: "white" }}>
+      <h1>My Car Requests</h1>
+      <p>You have no active requests.</p>
+    </div>
+  );
+}
