17ed59985b69909a9c310352c3a2d7f99c0e13ac
carbidx2025 <carbidx2025@gmail.com>
2025-11-14 22:05:24 -0800

    Create buyerauctions.jsx

---
+++ frontend/src/pages/Buyer/buyerauctions.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function BuyerAuctions() {
+  return (
+    <div style={{ padding: "30px", color: "white" }}>
+      <h1>My Auctions</h1>
+      <p>No active auctions available.</p>
+    </div>
+  );
+}
